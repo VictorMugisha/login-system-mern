a37a1fc514f2b10e1bc7c9a726cf63714c5d528a
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-03 19:30:54 +0200

    Moved login route handler into controller folder

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,5 +1,10 @@
 const UserModel = require("../model/userModel.js");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 async function registerUser(req, res) {
   try {
@@ -39,6 +44,42 @@ async function registerUser(req, res) {
   }
 }
 
-module.exports = {
-    registerUser
+// Login controller
+async function loginUser(req, res) {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({
+      $or: [{ username: username }, { email: username }],
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or email" });
+    }
+
+    const comparePasswords = await bcrypt.compare(password, user.password);
+    if (!comparePasswords) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const payload = {
+      userId: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, JWT_SECRET_KEY);
+
+    res
+      .status(201)
+      .json({ success: true, message: "User logged in successfully!", token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 }
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- server/server.js
+++ server/server.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
 const cors = require("cors");
-const UserModel = require("./model/userModel.js");
-const jwt = require("jsonwebtoken");
 
 // Controllers
-const { registerUser } = require("./controller/usersController.js");
+const { registerUser, loginUser } = require("./controller/usersController.js");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI;
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
@@ -28,36 +24,7 @@ mongoose
 app.post("/register", registerUser);
 
 // Login route handler
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await UserModel.findOne({
-      $or: [{ username: username }, { email: username }],
-    });
-
-    if (!user) {
-      return res.status(400).json({ message: "Invalid username or email" });
-    }
-
-    const comparePasswords = await bcrypt.compare(password, user.password);
-    if (!comparePasswords) {
-      return res.status(400).json({ message: "Incorrect password" });
-    }
-
-    const payload = {
-      userId: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      username: user.username,
-      email: user.email,
-    };
-    const token = jwt.sign(payload, JWT_SECRET_KEY);
-
-    res
-      .status(201)
-      .json({ success: true, message: "User logged in successfully!", token });
-  } catch (error) {}
-});
+app.post("/login", loginUser);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
