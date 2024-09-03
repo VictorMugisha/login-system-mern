eef112266dc858bb55c80e8b59999f3bec2ab9ac
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-03 14:11:43 +0200

    Created the register route handler

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const core = require("cors");
+const UserModel = require("./model/userModel.js");
 
 dotenv.config();
 const app = express();
@@ -11,3 +12,52 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB connection error: ", error));
+
+// ROUTE HANDLERS
+
+app.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, username, password } = req.body;
+    if (!firstName || !lastName || !email || !username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const existingUser = await UserModel.findOne($or[{ email }], [
+      { username },
+    ]);
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({
+      firstName,
+      lastName,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
