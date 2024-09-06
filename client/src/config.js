934beab6e15095e54b9d42e220f2b94159f0b4f5
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-06 14:06:04 +0200

    Used the deployed backend as BASE_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_API = "http://localhost:5000";
+// export const BASE_API = "http://localhost:5000";
 // export const BASE_API = "https://victor-login-system.onrender.com";
+export const BASE_API = "https://login-system-mern-2-jrdh.onrender.com/";
